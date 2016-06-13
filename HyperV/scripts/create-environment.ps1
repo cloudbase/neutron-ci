af49b3f3f3b62eae0f9600aeae09c2645b3908c8
Costin Galan <cgalan@cloudbasesolutions.com>
2016-06-13 17:07:30 +0300

    Get pip from the dl server
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -190,7 +190,7 @@ else
 }
 Add-Content "$env:APPDATA\pip\pip.ini" $pip_conf_content
 
-& easy_install -U pip
+& easy_install -f http://dl.openstack.tld:8080/cloudbase/CI/+simple/pip pip
 & pip install -U setuptools
 & pip install -U --pre pymi
 
